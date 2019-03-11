ce901037ccb19d91dcb047a0cf592ffa4c21fbdb
John Cho <johnycho16@gmail.com>
2019-03-11 16:10:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,11 @@
 # Write your code here!
+def game_hash
+  teams_roster = Hash.new
+  teams_roster[:home] = { :team_name => "Brooklin Nets", :colors => ["Black","White"], :players => { "Alan Anderson" => { :number => "0",:shoe => "16",:points => "22",:rebounds => "12",:assists => "12",:steals => "3",:blocks => "1",:slam_dunks => "1" },"Reggie Evans" => { :number => "30",:shoe => "14",:points => "12",:rebounds => "12",:assists => "12",:steals => "12",:blocks => "12",:slam_dunks => "7" }, "Brook Lopez" => { :number => "11",:shoe => "17",:points => "17",:rebounds => "19",:assists => "10",:steals => "3",:blocks => "1",:slam_dunks => "15" },"Mason Plumlee" => { :number => "1",:shoe => "19",:points => "26",:rebounds => "12",:assists => "6",:steals => "3",:blocks => "8",:slam_dunks => "5" },"Jason Terry" => { :number => "31",:shoe => "15",:points => "19",:rebounds => "2",:assists => "2",:steals => "4",:blocks => "11",:slam_dunks => "1" } }}
+  puts teams_roster
+end
 
-
-
+game_hash
 
 
 
